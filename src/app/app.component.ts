a9568fdb4e0faf7029a43b2d2ba29edde962f5fd
chris.w.newman <chris.w.newman@accenturefederal.com>
2018-01-18 22:52:05 -0500

    added nabber

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+  <nav class="navbar navbar-dark bg-dark navbar-space">
+    <span class="navbar-brand mb-0 h1">{{title}}</span>
+  </nav>
   <div class="container-fluid">
-    <h1>{{title}}</h1>
     <router-outlet></router-outlet>
   </div>
   `,
-  styles: []
+  styles: [`
+  .navbar-space{
+    margin-bottom: 13px;
+  }
+  
+  `]
 })
 export class AppComponent {
   title = 'MTG Deck Curve';
